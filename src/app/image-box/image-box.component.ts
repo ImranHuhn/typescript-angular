0ed74228a49e8a45689b44bf824b5151c9a1ca76
ImranRovano <imranhuhn1@gmail.com>
2023-02-04 23:01:36 -0500

    Property Binding Lesson

--- src/app/image-box/image-box.component.ts
+++ src/app/image-box/image-box.component.ts
@@ -10,6 +10,7 @@ import { GetDataService } from './get-data.service';
 export class ImageBoxComponent implements OnInit {
   contractInComponent;
   testingBoxInComponent = false;
+  coverUrlInComponent = 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
   constructor(service: 
     //ImageBoxService,
     GetDataService) {
