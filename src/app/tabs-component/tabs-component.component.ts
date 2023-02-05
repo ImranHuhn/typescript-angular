4477058eb79002be6c33b39b0461655499de1c30
ImranRovano <imranhuhn1@gmail.com>
2023-02-05 15:09:42 -0500

    Select Tab Logic Lesson

--- src/app/tabs-component/tabs-component.component.ts
+++ src/app/tabs-component/tabs-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs-component',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabs-component.component.scss']
 })
 export class TabsComponentComponent {
-
+  
+  activeTab = 'tab-1'
+  
+  constructor() {}
+  selectTab(tab: string) {
+    this.activeTab = tab;
+    console.log(tab);
+  }
+  ngOnInit() {}
 }
